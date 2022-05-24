fe463952f663d9d0df9d8804ed910242ce1edff2
DPLong <dplong742@gmail.com>
2022-05-24 17:17:32 -0400

    iterating on confirmations

--- script.js
+++ script.js
@@ -5,16 +5,15 @@
 // display in alert
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword)
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -28,34 +27,120 @@ function generatePassword() {
 
   var lengthInput = parseInt(prompt("Choose a length between 8 and 128"));
 
-
+  //length input check
   if (lengthInput >= 8 && lengthInput <= 128) {
     var lowerConfirm = confirm("Use lowercase letters?");
     var upperConfirm = confirm("Use uppercase letters?");
     var numConfirm = confirm("Use numbers?");
     var specialConfirm = confirm("Use special characters?");
-    var passwordCharacters = "";
+    var passCharAccepted = "";
 
+    //0 confirms check
     if (
       !lowerConfirm &&
       !upperConfirm &&
       !numConfirm &&
       !specialConfirm) {
       alert("Please select at least one character type.");
-
-    } else if (
-      lowerConfirm ||
-      upperConfirm ||
-      numConfirm ||
-      specialConfirm) {
-
-      //continue generator
-      console.log("continue please");
     }
 
+    //4 confirms
+    else if (
+      lowerConfirm &&
+      upperConfirm &&
+      numConfirm &&
+      specialConfirm) {
+      passCharAccepted = lowercase.concat(uppercase, numbers, special);
+    }
+
+    //3 confirms
+    else if (
+      lowerConfirm &&
+      upperConfirm &&
+      numConfirm &&
+      !specialConfirm) {
+      passCharAccepted = lowercase.concat(uppercase, numbers);
+    } else if (
+      lowerConfirm &&
+      upperConfirm &&
+      !numConfirm &&
+      specialConfirm) {
+      passCharAccepted = lowercase.concat(uppercase, special);
+    } else if (
+      lowerConfirm &&
+      !upperConfirm &&
+      numConfirm &&
+      specialConfirm) {
+      passCharAccepted = lowercase.concat(numbers, special);
+    } else if (
+      !lowerConfirm &&
+      upperConfirm &&
+      numConfirm &&
+      specialConfirm) {
+      passCharAccepted = uppercase.concat(numbers, special);
+    }
+
+    //2 confirms
+    else if (
+      lowerConfirm &&
+      upperConfirm &&
+      !numConfirm &&
+      !specialConfirm) {
+      passCharAccepted = lowercase.concat(uppercase);
+    } else if (
+      !lowerConfirm &&
+      upperConfirm &&
+      numConfirm &&
+      !specialConfirm) {
+      passCharAccepted = uppercase.concat(numbers);
+    } else if (
+      !lowerConfirm &&
+      !upperConfirm &&
+      numConfirm &&
+      specialConfirm) {
+      passCharAccepted = numbers.concat(special);
+    } else if (
+      lowerConfirm &&
+      !upperConfirm &&
+      !numConfirm &&
+      specialConfirm) {
+      passCharAccepted = lowercase.concat(special);
+    }
+
+    //1 confirm
+    else if (
+      lowerConfirm &&
+      !upperConfirm &&
+      !numConfirm &&
+      !specialConfirm) {
+      passCharAccepted = lowercase;
+    } else if (
+      !lowerConfirm &&
+      upperConfirm &&
+      !numConfirm &&
+      !specialConfirm) {
+      passCharAccepted = uppercase;
+    } else if (
+      !lowerConfirm &&
+      !upperConfirm &&
+      numConfirm &&
+      !specialConfirm) {
+      passCharAccepted = numbers;
+    } else if (
+      !lowerConfirm &&
+      !upperConfirm &&
+      !numConfirm &&
+      specialConfirm) {
+      passCharAccepted = special;
+    }
+
+    for (var i = 0; i < lengthInput; i++) {
+
+    }
   } else {
     alert("Please enter a valid length.");
-  } generatePassword();
+  }
 
   return "";
 }
+
